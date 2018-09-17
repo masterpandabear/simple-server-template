46bf6cfcb6375ab59341095388212a6d32396d82
Alberto Arias <7119875+masterpandabear@users.noreply.github.com>
2018-09-17 16:56:10 -0500

    Adds basic error handling for express

--- app.js
+++ app.js
@@ -5,4 +5,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const errorHandler = (error, request, response, next) => {
+  const statusCode = error.statusCode || 500;
+  response.locals.message = error.message;
+  response.locals.error = request.app.get('env') === 'development' ? error : {};
+  const messageToReturn = error.message;
+  response.status(statusCode);
+  response.json({
+    message: messageToReturn,
+    code: error.code || error.errorCode,
+  });
+};
+
+app.use((request, response, next) => {
+  const error = new Error('Not found');
+  error.statusCode = 404;
+  next(error);
+});
+
+app.use(errorHandler);
+
 module.exports = app;
