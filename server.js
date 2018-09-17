15d21afaa6cdc58104ca75fc2e67da5282e55221
Alberto Arias <7119875+masterpandabear@users.noreply.github.com>
2018-09-17 16:43:36 -0500

    Adds the linter configured with airbnb styleguide

--- server.js
+++ server.js
@@ -3,8 +3,28 @@
  * Module dependencies.
  */
 
-const app = require('./app');
 const http = require('http');
+const app = require('./app');
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(value) {
+  const port = parseInt(value, 10);
+
+  if (Number.isNaN(port)) {
+    // named pipe
+    return value;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
 
 /**
  * Get port from environment and store in Express.
@@ -24,28 +44,6 @@ const server = http.createServer(app);
  */
 
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
 
 /**
  * Event listener for HTTP server "error" event.
@@ -56,18 +54,16 @@ function onError(error) {
     throw error;
   }
 
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+      console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+      console.error(`${bind} + ' is already in use`);
       process.exit(1);
       break;
     default:
@@ -81,8 +77,9 @@ function onError(error) {
 
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-    console.log(`Running at ${bind}`);
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+  console.log(`Running at ${bind}`);
 }
+
+server.on('error', onError);
+server.on('listening', onListening);
