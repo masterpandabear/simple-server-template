e82b47ac08374a5a12b609fdf8387560148e26b0
Alberto Arias <7119875+masterpandabear@users.noreply.github.com>
2018-09-18 16:50:35 -0500

    Adds routes structure

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { logger, expressLogger } = require('./application/loggers');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(expressLogger);
+app.use('/', routes);
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (error, request, response, next) => {
